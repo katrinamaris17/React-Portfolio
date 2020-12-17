c58b13f83107fe40ee3ba2991eafb3935a68a7ed
Katrina Arando <katrinamaris17@gmail.com>
2020-12-16 23:50:35 -0800

    added social media links to contact and updated readme

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -23,7 +23,6 @@ function AboutMe() {
             I am currently learning full-stack web development at the UC Berkeley Coding Boot Camp. I have over 10+ years of successful project management experience in the construction industry. I have a passion for learning new things, solving problems and bringing visions to life. My goal is to parlay my years of professional experience and skill sets into the world of web design and software development. I live in the Bay Area with my family. We love exploring new places together. Please feel free to connect with me through the contact page.
             </p>
             <br />
-            <i className="fab fa-instagram"></i>
             </Col>
         </Row>
         </Container>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,38 +1,70 @@
-import React from 'react';
-import { Jumbotron , Container, Col, Row, Form } from 'react-bootstrap';
-import {
-  Link
-} from "react-router-dom";
+import React from "react";
+import { Jumbotron, Container, Col, Row, Form, Button } from "react-bootstrap";
+import { MDBIcon, MDBContainer, MDBBtn } from "mdbreact";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { Link } from "react-router-dom";
 
 function Contact() {
-    return (
-        <Container style={{backgroundColor:"#98B4D4"}}>
-          <br></br>
-          <Row>
-          <Col></Col>
-          <Col xs={6} style={{textAlign:"center", fontStyle:"inherit"}}>Contact Me</Col>
-          <Col></Col>
-          </Row>
-          <br></br>
-        <Row>
-          <Col></Col>
-          <Col xs={6}>
+  return (
+    <Container style={{ backgroundColor: "#98B4D4" }}>
+      <br></br>
+      <Row>
+        <Col></Col>
+        <Col xs={6} style={{ textAlign: "center", fontStyle: "inherit" }}>
+          Contact Me
+        </Col>
+        <Col></Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col></Col>
+        <Col xs={6}>
           <Form>
             <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
             </Form.Group>
-            </Form>
-          </Col>
-          <Col></Col>
-        </Row>
-        <br></br>
-      </Container>
-    )
+            <MDBContainer></MDBContainer>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col></Col>
+        <Col xs={6} style={{ textAlign: "center", fontStyle: "inherit" }}>
+          Connect With Me Here
+        </Col>
+        <Col></Col>
+      </Row>
+      <br></br>
+      <Row>
+      <Col></Col>
+        <Col xs={6} style={{ textAlign: "center", fontStyle: "inherit" }}>
+          <MDBBtn
+            social="ins"
+            onClick={(e) =>(window.location = "https://www.instagram.com/katrinamaris17/")}>
+            <MDBIcon fab icon="instagram" className="pr-1" /> Instagram</MDBBtn>
+          <MDBBtn
+            social="li"
+            onClick={(e) =>(window.location ="https://www.linkedin.com/in/katrina-arando-0b4a7a26/")}>
+          <MDBIcon fab icon="linkedin-in" className="pr-1" /> Linkedin</MDBBtn>
+          <MDBBtn social="git"
+            onClick={(e) =>(window.location ="https://github.com/katrinamaris17")}>
+          <MDBIcon fab icon="github" className="pr-1" /> Github
+        </MDBBtn>
+        </Col>
+        <Col></Col>
+      </Row>
+      <br></br>
+    </Container>
+  );
 }
 
 export default Contact;
